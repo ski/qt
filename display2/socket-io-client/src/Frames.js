1799439e0ef25adfd919311877eee044b09262a4
Suhail Manzoor <suhailski@gmail.com>
2019-05-14 22:31:23 +0400

    Frame selection corrected

--- display2/socket-io-client/src/Frames.js
+++ display2/socket-io-client/src/Frames.js
@@ -1,22 +1,49 @@
 
 import React, { Component } from "react";
 import './App.css';
-import f0 from "./images/f0.png";
-import f1 from "./images/f1.png";
-import f2 from "./images/f2.png";
-import f3 from "./images/f3.png";
+import v0 from "./images/f0.png";
+import v1 from "./images/f1.png";
+import v2 from "./images/f2.png";
+import v3 from "./images/f3.png";
 
 class Frames extends Component {
 
+    rone = (sel) => {        
+        let row = [];
+        let one = [{v0}, {v1}];
+        for (let i = 0; i < one.length; i++) {
+            if(sel == i){                
+                row.push(<img id='selected' src={one[i]['v'+i]} />);
+            } else {
+                row.push(<img src={one[i]['v'+i]} />);
+            }
+        }
+        return row;
+    }
+
+    rtwo = (sel) => {
+        let row = [];
+        let one = [{v2}, {v3}];
+        console.log(one); 
+        for (let i = 2; i < 4; i++) {  
+            console.log(i); 
+            if(sel == i){                
+                row.push(<img id='selected' src={one[i-2]['v'+i]} />);
+            } else {
+                row.push(<img src={one[i-2]['v'+i]} />);
+            }
+        }
+        return row;
+    }
+
     render() {
+        const {esel} = this.props;
         return(<div className="row">
             <div className="column">
-                <img src={f0} />
-                <img src={f1} />
+                 {this.rone(esel)}
             </div>
             <div className="column">
-                <img id='selected' src={f2} />
-                <img src={f3} />
+                {this.rtwo(esel)}
             </div>
         </div>
     );
