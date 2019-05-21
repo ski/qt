dbd0544111be4a1284ac7b620de68bc312f5cac4
Suhail Manzoor <suhailski@gmail.com>
2019-05-21 11:21:25 +0400

    server changes

--- display1/api/controllers/controller.js
+++ display1/api/controllers/controller.js
@@ -2,12 +2,17 @@
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
+//batch a
 // var d2 = "http://192.168.1.104:3002";
 // var d3 = "http://192.168.1.126:3005";
 
+//batch b
+var d2 = "http://192.168.1.127:3002";
+var d3 = "http://192.168.1.137:3005";
 
-var d2 = "http://localhost:3002";
-var d3 = "http://localhost:3005";
+
+// var d2 = "http://localhost:3002";
+// var d3 = "http://localhost:3005";
 
 //http://localhost:3003/sync?two=c21&three=c31&sel=5
 //http://localhost:3005/sync?three=c31&&sel=5
